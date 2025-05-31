790726c2d863379fcc7e52c945ff120a5b4c4cd9
Paul Garell <prgarell@gmail.com>
2025-05-31 01:28:27 -0400

    Give app purpose

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1> Hello Parker!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+         This definitely is <s> not</s> a useless button.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        made by the goat
       </p>
     </>
   )
